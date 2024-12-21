b6ec11ddfb710598dbd11f0db1b4d690f92e42ee
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2024-12-21 15:26:38 +0200

    Ported time

--- src/Platform_MRE.c
+++ src/Platform_MRE.c
@@ -1,6 +1,8 @@
 #include "Core.h"
 #if defined CC_BUILD_MRE
 
+#include "_PlatformBase.h"
+#include "Utils.h"
 #include <vmsys.h>
 
 /*########################################################################################################################*
@@ -27,6 +29,34 @@ void* Mem_TryRealloc(void* mem, cc_uint32 numElems, cc_uint32 elemsSize) {
 
 void Mem_Free(void* mem) {
 	if (mem) vm_free(mem);
+}
+
+/*########################################################################################################################*
+*------------------------------------------------------Logging/Time-------------------------------------------------------*
+*#########################################################################################################################*/
+
+void Platform_Log(const char* msg, int len) {
+	int ret;
+	/* Avoid "ignoring return value of 'write' declared with attribute 'warn_unused_result'" warning */
+	//TODO
+}
+
+TimeMS DateTime_CurrentUTC(void) {
+	VMUINT sec = 0;
+	vm_get_utc(&sec);
+	return (cc_uint64)sec + UNIX_EPOCH_SECONDS;
+}
+
+void DateTime_CurrentLocal(struct cc_datetime* t) {
+	vm_time_t time;
+	vm_get_time(&time);
+
+	t->year = time.year;
+	t->month = time.mon;
+	t->day = time.day;
+	t->hour = time.hour;
+	t->minute = time.min;
+	t->second = time.sec;
 }
 
 #endif
